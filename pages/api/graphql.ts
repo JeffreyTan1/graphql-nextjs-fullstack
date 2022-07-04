bdd099ce6c3717c48a93ba6bb7859bad6353d206
Jeffrey Tan <jeff.tan@propella.ai>
2022-07-04 16:04:16 +1000

    init project with graphql

---
+++ pages/api/graphql.ts
@@ -0,0 +1,40 @@
+import "reflect-metadata";
+import { ApolloServer } from 'apollo-server-micro';
+import { buildSchema, Resolver, Query, Arg, ObjectType, Field, ID } from "type-graphql";
+
+@ObjectType()
+export class Dog {
+  @Field(() => ID)
+  name: string;
+
+}
+
+
+@Resolver(Dog)
+export class DogsResolver {
+    @Query(() => [Dog])
+    dogs(): Dog[] {
+        return [{ name: "Tilly" }, { name: "Daisy" }];
+    }
+}
+
+const schema = await buildSchema({
+    resolvers: [DogsResolver]
+})
+
+const server = new ApolloServer({
+    schema,
+});
+
+export const config = {
+    api: {
+        bodyParser: false,
+    }
+};
+
+const startServer = server.start();
+    
+export default async function handler(req, res) {
+    await startServer;
+    return server.createHandler({ path: '/api/graphql' })(req, res);
+}
